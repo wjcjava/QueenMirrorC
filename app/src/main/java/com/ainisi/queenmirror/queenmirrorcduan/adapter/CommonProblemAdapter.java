78b9eeb95ffec40a42414c7d455ce86d5b95e5ef
john <669851>
2018-05-28 12:50:44 +0800

    增加部分UI
    Signed-off-by: john <669851>

--- app/src/main/java/com/ainisi/queenmirror/queenmirrorcduan/adapter/CommonProblemAdapter.java
+++ app/src/main/java/com/ainisi/queenmirror/queenmirrorcduan/adapter/CommonProblemAdapter.java
@@ -12,6 +12,7 @@ import com.ainisi.queenmirror.queenmirrorcduan.api.HttpUtils;
 import com.ainisi.queenmirror.queenmirrorcduan.ui.mine.bean.ProblemDataBean;
 import com.ainisi.queenmirror.queenmirrorcduan.ui.mine.bean.ProblemListBean;
 import com.ainisi.queenmirror.queenmirrorcduan.utilnomal.GsonUtil;
+import com.ainisi.queenmirror.queenmirrorcduan.utilnomal.T;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.lzy.okgo.cache.CacheMode;
@@ -21,58 +22,61 @@ import java.util.List;
 
 /**
  * Created by john on 2018/5/26.
+ *
  */
 
-public class CommonProblemAdapter extends BaseQuickAdapter<ProblemListBean.BodyBean.QaCateListDataBean, BaseViewHolder> implements HttpCallBack {
-
-    private TextView answer;
+public class CommonProblemAdapter extends BaseQuickAdapter<ProblemListBean.BodyBean.QaCateListDataBean, BaseViewHolder> implements HttpCallBack{
+    private String qaQuestion;
 
     public CommonProblemAdapter(int layoutResId, @Nullable List<ProblemListBean.BodyBean.QaCateListDataBean> data) {
         super(layoutResId, data);
+
     }
 
     @Override
     protected void convert(BaseViewHolder helper, ProblemListBean.BodyBean.QaCateListDataBean item) {
         helper.setText(R.id.problem_name, item.getLabel());
         final CheckBox check_problem = helper.getView(R.id.check_problem);
-        answer = helper.getView(R.id.tv_answer);
+        final TextView answer = helper.getView(R.id.tv_answer);
+        inithttp();
         check_problem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (check_problem.isChecked()) {
+                if(check_problem.isChecked()){
+
                     answer.setVisibility(View.VISIBLE);
-                    inithttp();
-                } else {
+                    answer.setText(qaQuestion);
+
+                }else {
                     answer.setVisibility(View.GONE);
                 }
             }
         });
     }
-
     private void inithttp() {
-        HashMap<String, String> parames = new HashMap<>();
-        parames.put("qaCategory", "1");
-        HttpUtils.doPost(ACTION.PROBLEMDATA, parames, CacheMode.REQUEST_FAILED_READ_CACHE, true, this);
+        HashMap<String,String> parames=new HashMap<>();
+        parames.put("qaCategory","1");
+        HttpUtils.doPost(ACTION.PROBLEMDATA,parames, CacheMode.REQUEST_FAILED_READ_CACHE,true,this);
     }
 
     @Override
     public void onSuccess(int action, String res) {
-        switch (action) {
-            case ACTION.PROBLEMDATA:
-                ProblemDataBean dataBean = GsonUtil.toObj(res, ProblemDataBean.class);
-                if(dataBean.isSuccess()){
-                    List<ProblemDataBean.BodyBean.QaListDataBean> dataList = dataBean.getBody().getQaListData();
-                    answer.setText(dataList.get(0).getEcQa().getQaQuestion());
-                }
-                break;
-        }
+            switch (action){
+                case ACTION.PROBLEMDATA:
+                   ProblemDataBean dataBean=GsonUtil.toObj(res, ProblemDataBean.class);
+                   if(dataBean.isSuccess()){
+                       qaQuestion=dataBean.getBody().getQaListData().get(0).getEcQa().getQaQuestion();
+                   }else {
+                       T.show(dataBean.getMsg());
+                   }
+                    break;
+            }
     }
 
     @Override
     public void showLoadingDialog() {
 
     }
-
     @Override
     public void showErrorMessage(String s) {
 
